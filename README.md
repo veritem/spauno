3ea6672f66849af4aa7441bba453e9a7f9ef3cd0
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-13 22:53:33 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#### spauno
+
+##### Getting Started
+
+- [ ] clone the repo
+- [ ] install the dependencies
+- [ ] run `pnpm dev`
+- [ ] open browser at `http:://localhost:3000`
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
+// import { useStateResults, useSearchState } from "react-instantsearch/hooks";
+import { Fragment } from "react";
+import Nav from "./components/Nav";
+
+// function Hit({ hit }: {
+//   hit: {
+//     image: string;
+//     name: string;
+//     price: number;
+//     categories: string[];
+//     title: string;
+//   }
+// }) {
+//   return <article>
+//     <img src={hit.image} alt={hit.title} />
+//     <p>{hit.categories[0]}</p>
+//     <h1>{hit.name}</h1>
+//     <p>${hit.price}</p>
+//   </article>
+// }
+
+
+// https://github.com/learnwithjason/javascript-autocomplete/blob/main/src/main.jsx
+// https://www.algolia.com/doc/guides/building-search-ui/what-is-instantsearch/react-hooks/
+
 
 function App() {
 
+  // const searchClient = algoliasearch("NBT0N27ACR", "2b05e9374735ebf83d8190cb33bd832b");
+
+  const { query, results, loading, setQuery } = useSearch("NBT0N27ACR", "2b05e9374735ebf83d8190cb33bd832b")
+
   return (
-    <div className="App">
-      <h1 className="bg-green">CodeSandbox</h1>
-    </div>
-  )
+    <Fragment>
+      <Nav />
+
+      {/* <InstantSearch searchClient={searchClient} indexName="tights_posts_product" > */}
+      {/* <SearchBox autoFocus={true} /> */}
+      {/* <RefinementList attribute="categories" /> */}
+      {/* <Hits hitComponent={Hit} /> */}
+      {/* </InstantSearch> */}
+
+      <input type="search" onChange={e => { setQuery(e.target.value) }} />
+      {results.length > 0 ? (
+        // <Results query={query} results={results} />
+        <p>Hello</p>
+      ) : (
+        loading
+          ? "Loading…"
+          : query.length > 0 && `No results for “${query}”.`
+      )}
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
